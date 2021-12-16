47539a27147f1b42b826f6abab64c80e0426af54
yud0uhu <cist.b219.y.osaka@gmail.com>
2021-12-16 09:05:16 +0900

    fetat: first commit

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,27 @@
 <template>
-  <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
-  </div>
+  <div id="app"></div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import "leaflet/dist/leaflet.css";
+import L from "leaflet";
 
 export default {
-  name: 'App',
-  components: {
-    HelloWorld
-  }
-}
+  mounted() {
+    L.map("app", { center: L.latLng(35.6825, 139.752778), zoom: 15 }).addLayer(
+      L.tileLayer("http://{s}.tile.osm.org/{z}/{x}/{y}.png")
+    );
+  },
+};
 </script>
 
 <style>
+html,
+body,
 #app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+  height: 100%;
+}
+body {
+  margin: 0;
 }
 </style>
