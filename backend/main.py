58cbb844efe556658e717873ac83a5c1993c18d4
yud0uhu <cist.b219.y.osaka@gmail.com>
2022-02-15 08:21:23 +0900

    fix: DB情報をマスク,Herokuでエンドポイントの動作確認ができる形に修 正

--- backend/main.py
+++ backend/main.py
@@ -3,22 +3,33 @@ from typing import List
 import databases
 import sqlalchemy
 from fastapi import FastAPI
+from fastapi.encoders import jsonable_encoder
+from fastapi.responses import JSONResponse
 from pydantic import BaseModel
+from datetime import datetime
+from starlette.middleware.cors import CORSMiddleware
+
+import os
+from dotenv import load_dotenv
+load_dotenv()
 
 # SQLAlchemy specific code, as with any other app
 # DATABASE_URL = "sqlite:///./test.db"
-DATABASE_URL = "postgresql://osxhgvdnwkrrsx:03aa7fc38ea3842c590df8e451a160fc593eb770287a3440ff005797b79b1125@ec2-34-230-110-100.compute-1.amazonaws.com:5432/d9393lnt8ndre9"
+DATABASE_URL = os.getenv('DATABASE_URL')
 
 database = databases.Database(DATABASE_URL)
 
 metadata = sqlalchemy.MetaData()
 
-notes = sqlalchemy.Table(
-    "notes",
+ledgers = sqlalchemy.Table(
+    "ledger",
     metadata,
-    sqlalchemy.Column("id", sqlalchemy.Integer, primary_key=True),
-    sqlalchemy.Column("text", sqlalchemy.String),
-    sqlalchemy.Column("completed", sqlalchemy.Boolean),
+    sqlalchemy.Column("order_no", sqlalchemy.String, primary_key=True),
+    sqlalchemy.Column("datatime", sqlalchemy.DateTime),
+    sqlalchemy.Column("primary_category", sqlalchemy.String),
+    sqlalchemy.Column("secondary_category", sqlalchemy.String),
+    sqlalchemy.Column("contents", sqlalchemy.String),
+    sqlalchemy.Column("answer", sqlalchemy.String),
 )
 
 
@@ -29,38 +40,72 @@ engine = sqlalchemy.create_engine(
 metadata.create_all(engine)
 
 
-class NoteIn(BaseModel):
-    text: str
-    completed: bool
+class LedgerIn(BaseModel):
+    order_no: str
+    datatime: datetime
+    primary_category: str
+    secondry_category: str
+    contents: str
+    answer: str
 
 
-class Note(BaseModel):
-    id: int
-    text: str
-    completed: bool
+class Ledger(BaseModel):
+    order_no: str
+    datatime: datetime
+    primary_category: str
+    secondry_category: str
+    contents: str
+    answer: str
 
 
 app = FastAPI()
 
 
-@app.on_event("startup")
-async def startup():
-    await database.connect()
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],
+    allow_credentials=True, 
+    allow_methods=["*"],
+    allow_headers=["*"]
+)
 
 
-@app.on_event("shutdown")
-async def shutdown():
-    await database.disconnect()
+
+@app.get("/")
+async def root():
+    return {"message": "Hello World"}
 
 
-@app.get("/notes/", response_model=List[Note])
-async def read_notes():
-    query = notes.select()
-    return await database.fetch_all(query)
+
+# @app.on_event("startup")
+# async def startup():
+#     await database.connect()
 
 
-@app.post("/notes/", response_model=Note)
-async def create_note(note: NoteIn):
-    query = notes.insert().values(text=note.text, completed=note.completed)
-    last_record_id = await database.execute(query)
-    return {**note.dict(), "id": last_record_id}
+# @app.on_event("shutdown")
+# async def shutdown():
+#     await database.disconnect()
+
+
+@app.get("/ledger/{id}/", response_model=Ledger)
+async def read_ledgers(id: str):
+    # query = ledgers.select()
+    query = "SELECT * FROM ledger WHERE order_no=(%s)"
+    with engine.connect() as connection:
+        result = connection.execute(query,id)
+        for r in result:
+            return JSONResponse(content={
+                    "order_no": r[0],
+                    "datatime": str(r[1]),
+                    "primary_category": r[2],
+                    "secondry_category": r[3],
+                    "contents": r[4],
+                    "answer": r[5]
+                    })
+
+# @app.post("/ledger/", response_model=Ledger)
+# async def create_ledger(ledger: LedgerIn):
+#     query = ledgers.insert().values(text=ledger.text, completed=ledger.completed)
+#     last_record_id = await database.execute(query)
+#     return {**ledger.dict(), "orde_no": last_record_id}
